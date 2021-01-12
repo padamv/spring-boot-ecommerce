6f3bfd6c426980ff0caf387ca96d33d5861f6bc3
padamv <padamv91@gmail.com>
2021-01-12 10:16:09 +0100

    Create states entity and set relationship between country and state

--- src/main/java/com/luv2code/ecommerce/entity/Country.java
+++ src/main/java/com/luv2code/ecommerce/entity/Country.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="country")
@@ -22,5 +23,6 @@ public class Country {
     @Column(name="name")
     private String name;
 
-    // TODO: Set up one-to-many with states
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
 }
